a629da97447a4970fe0ba85285818c7897a83538
Tommie Nygren <tommie.nygren@gmail.com>
2021-12-15 09:51:37 +0100

    Slime entity coming along nicely

--- core/src/main/kotlin/core/FirstScreen.kt
+++ core/src/main/kotlin/core/FirstScreen.kt
@@ -1,15 +1,16 @@
 package core
 
 import com.badlogic.ashley.core.Engine
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import core.Factories.ball
-import core.Factories.blobEntity
 import core.Factories.platform
 import injection.Context.inject
+import ktx.app.KtxInputAdapter
 import ktx.app.KtxScreen
 import ktx.assets.disposeSafely
 import ktx.assets.toInternalFile
@@ -25,12 +26,15 @@ val engine: Engine
         return inject()
     }
 
+object ControlObject {
+    val directionVector = vec2()
+}
 
 
 class FirstScreen(
     private val engine: Engine,
     private val viewPort: ExtendViewport,
-    private val camera: OrthographicCamera) : KtxScreen {
+    private val camera: OrthographicCamera) : KtxScreen, KtxInputAdapter {
     private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(
         Texture.TextureFilter.Linear,
         Texture.TextureFilter.Linear
@@ -39,12 +43,18 @@ class FirstScreen(
 
     override fun show() {
         ball(vec2(5f,5f))
-        blobEntity(vec2(0f, 0f), 10f)
-        platform(vec2(-10f,-20f), 40f, 2.5f)
+        //blobEntity(vec2(0f, 0f), 10f)
+        buildSlimeCircle(vec2(0f, 0f))
+        platform(vec2(-20f,-50f), 100f, 2.5f)
     }
 
     override fun render(delta: Float) {
+        updateDirectionVector()
         engine.update(delta)
+    }
+
+    private fun updateDirectionVector() {
+        ControlObject.directionVector.set(x,y).nor()
     }
 
     override fun resize(width: Int, height: Int) {
@@ -56,5 +66,54 @@ class FirstScreen(
     override fun dispose() {
         image.disposeSafely()
         batch.disposeSafely()
+    }
+
+    /**
+     * Deal with some inputs
+     */
+    var x = 0f
+    var y = 0f
+    override fun keyDown(keycode: Int): Boolean {
+        return when(keycode) {
+            Input.Keys.W -> {
+                y = 1f
+                true
+            }
+            Input.Keys.A -> {
+                x = -1f
+                true
+            }
+            Input.Keys.S -> {
+                y = -1f
+                true
+            }
+            Input.Keys.D -> {
+                x = 1f
+                true
+            }
+            else -> super.keyDown(keycode)
+        }
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        return when(keycode) {
+            Input.Keys.W -> {
+                y = 0f
+                true
+            }
+            Input.Keys.A -> {
+                x = 0f
+                true
+            }
+            Input.Keys.S -> {
+                y = 0f
+                true
+            }
+            Input.Keys.D -> {
+                x = 0f
+                true
+            }
+            else -> super.keyDown(keycode)
+        }
     }
 }
